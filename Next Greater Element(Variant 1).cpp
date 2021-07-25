538e330694c2fdf9a4ee1fb447b43d5b0b5bb4ba
SayanDutta001 <84002255+SayanDutta001@users.noreply.github.com>
2021-07-25 13:30:47 +0530

    Create Next Greater Element(Variant 1).cpp

---
+++ Next Greater Element(Variant 1).cpp
@@ -0,0 +1,29 @@
+void nextGreatest(int *arr,int n){
+    stack<int> nge;
+    stack<int>ans;
+    for(int i=n-1;i>=0;i--){
+        
+        while(!nge.empty() and arr[i] >=nge.top())
+            nge.pop();
+        
+        (nge.empty()) ? ans.push(-1): ans.push(nge.top());
+        nge.push(*(arr+i));
+    }
+
+    while(!ans.empty()){
+        cout<<ans.top()<<" ";
+        ans.pop();
+    }
+    return;
+}
+
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	nextGreatest(arr,n);
+  return 0;
+}
